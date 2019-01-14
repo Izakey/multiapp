c26cd4ad9081e0357e4ea53dbb74847eca8827c9
Isaac Kamga <u2isaac@gmail.com>
2019-01-15 00:12:14 +0100

    The commons library for multiapp project

---
+++ commons/src/main/java/com/isaac/commons/CommonsApplication.java
@@ -0,0 +1,9 @@
+package com.isaac.commons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsApplication {
+  public static void main(String... strings) throws Exception {}
+}
+
